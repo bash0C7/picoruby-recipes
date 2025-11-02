64128e34d3e6a00f19e36bea3a194ca2fd4ff685
bash0C7 <ksb.4038.nullpointer+github@gmail.com>
2025-11-02 09:39:30 +0900

    Revert all refactoring changes from this session back to 2a52beb

--- src_components/R2P2-ESP32/storage/home/app.rb
+++ src_components/R2P2-ESP32/storage/home/app.rb
@@ -3,154 +3,116 @@ require 'ws2812'
 require 'i2c'
 require 'mpu6886'
 
-# ===== 定数定義 =====
+GT = {36=>1, 38=>2, 39=>3, 49=>5, 52=>5}
+HUES = [nil, 0, 128, 192, 64, 0]
 
-# MIDI
-NOTE_ON = 0x99
-CC = 0xB9
-VEL_MAX = 0x7F
-PROG_CHG = 0xC9
+pc_uart = UART.new(unit: :ESP32_UART0, baudrate: 115200)
+sleep_ms(10)
+md_uart = UART.new(unit: :ESP32_UART1, baudrate: 31250, txd_pin: 23, rxd_pin: 33)
+sleep_ms(10)
 
-# LED
-LED_COUNT = 60
-INIT_COLOR = 0xC0960A
-MAX_BRIGHT = 0x00FF
+led_strip = WS2812.new(RMTDriver.new(22))
+led_colors = Array.new(60, 0xC0960A)
+sleep_ms(10)
 
-# ビット演算
-HUE_SHIFT = 16
-SAT_SHIFT = 8
-HUE_MASK = 0xFF0000
+i2c_bus = I2C.new(unit: :ESP32_I2C0, frequency: 100_000, sda_pin: 25, scl_pin: 21)
+sleep_ms(100)
+accel_sensor = MPU6886.new(i2c_bus)
+sleep_ms(100)
+accel_sensor.accel_range = MPU6886::ACCEL_RANGE_2G
+sleep_ms(100)
 
-# ===== Groupクラス定義 =====
+pc_uart.clear_rx_buffer
+md_uart.clear_rx_buffer
+md_uart.write((0xC9).chr + (25).chr)
 
-class Group
-  @@groups = {}
-  @@note_map = {}
-  @@rotation = []
-  @@offset = 0
-  attr_reader :name, :hue
+button = GPIO.new(39, GPIO::IN|GPIO::PULL_UP)
+last_button_state = 1
+button_debounce_count = 0
+cymbal_trigger = {flag: false}
 
-  def initialize(name, hue)
-    @name = name
-    @hue = hue
-  end
+tick_count = 0
+group_history = [1, 1, 1]
+saturation = 255
+brightness = 111
+led_offset = 0
 
-  def self.create!(name, hue, notes: [], rotatable: true, &block)
-    g = new(name, hue)
-    g.define_singleton_method(:apply_leds, block) if block
-    @@groups[name] = g
-    notes.each { |n| @@note_map[n] = name }
-    @@rotation << g if rotatable
-    g
-  end
+loop do
+  tick_count += 1
 
-  def self.from_note(note)
-    name = @@note_map[note]
-    name ? @@groups[name] : @@groups[:default]
-  end
+  while pc_uart.bytes_available > 0
+    uart_data = pc_uart.read(1)
+    next unless uart_data && uart_data.length == 1
+    cmd_byte = uart_data[0].ord
 
-  def self.[](name)
-    @@groups[name]
-  end
-
-  def self.rotate(tick)
-    @@rotation[tick % @@rotation.size]
-  end
-
-  def self.offset
-    @@offset
-  end
-
-  def self.advance_offset!
-    @@offset = (@@offset + 1) % LED_COUNT
-  end
-
-  def apply_leds(colors, hsb)
-    puts "Unknown group: name=#{@name}, hue=#{@hue}, hsb=#{hsb}"
-  end
-end
-
-# グループ定義
-Group.create!(:kick, 0, notes: [36]) { |colors, hsb| 10.times { |s| (0..2).each { |i| colors[(s * 6 + i + Group.offset) % LED_COUNT] = hsb } } }
-Group.create!(:snare, 128, notes: [38]) { |colors, hsb| 10.times { |s| (3..5).each { |i| colors[(s * 6 + i + Group.offset) % LED_COUNT] = hsb } } }
-Group.create!(:clap, 192, notes: [39]) { |colors, hsb| 12.times { |i| colors[(i * 5 + Group.offset) % LED_COUNT] = hsb } }
-Group.create!(:crash, 0, notes: [49, 52], rotatable: false)
-Group.create!(:default, 64) { |colors, hsb| 6.times { |i| colors[(i * 10 + Group.offset) % LED_COUNT] = hsb } }
-
-# ===== ハードウェア初期化 =====
-
-ctrl, synth, leds, colors, accel, button = ->(led_pin = 22) {
-  ctrl = UART.new(unit: :ESP32_UART0, baudrate: 115200)
-  sleep_ms(10)
-  synth = UART.new(unit: :ESP32_UART1, baudrate: 31250, txd_pin: 23, rxd_pin: 33)
-  sleep_ms(10)
-
-  leds = WS2812.new(RMTDriver.new(led_pin))
-  colors = Array.new(LED_COUNT, INIT_COLOR)
-  sleep_ms(10)
-
-  i2c_bus = I2C.new(unit: :ESP32_I2C0, frequency: 100_000, sda_pin: 25, scl_pin: 21)
-  sleep_ms(100)
-  accel = MPU6886.new(i2c_bus)
-  sleep_ms(100)
-  accel.accel_range = MPU6886::ACCEL_RANGE_2G
-  sleep_ms(100)
-
-  ctrl.clear_rx_buffer
-  synth.clear_rx_buffer
-  synth.write(PROG_CHG.chr + 25.chr)
-
-  button = GPIO.new(39, GPIO::IN|GPIO::PULL_UP)
-
-  [ctrl, synth, leds, colors, accel, button]
-}.call
-
-debounce = 0
-hist = [Group[:kick], Group[:kick], Group[:kick]]
-sat = 255
-bright = 111
-
-1.step do |tick|
-
-  while ctrl.bytes_available > 0
-    d = ctrl.read(1)
-    next unless d && d.length == 1
-    cmd = d[0].ord
-
-    case cmd
+    case cmd_byte
     when 36..56
-      synth.write(NOTE_ON.chr + cmd.chr + VEL_MAX.chr)
-      hist.shift
-      hist << Group.from_note(cmd)
-      Group.advance_offset!
+      md_uart.write((0x99).chr + cmd_byte.chr + (0x7F).chr)
+      g = GT[cmd_byte] || 4
+      group_history.shift
+      group_history.push(g)
+      led_offset = (led_offset + 1) % led_colors.size
     when 1..10
-      synth.write(CC.chr + 91.chr + (((cmd - 1) * 127 / 9).to_i).chr)
+      md_uart.write((0xB9).chr + 91.chr + (((cmd_byte - 1) * 127 / 9).to_i).chr)
     when 11..20
-      synth.write(CC.chr + 93.chr + (((cmd - 11) * 127 / 9).to_i).chr)
+      md_uart.write((0xB9).chr + 93.chr + (((cmd_byte - 11) * 127 / 9).to_i).chr)
     end
   end
 
-  if button.read == 0 && debounce == 0
-    synth.write(NOTE_ON.chr + 49.chr + VEL_MAX.chr)
-    hist[-1] = Group[:crash]
-    debounce = 50
+  current_button = button.read
+  if last_button_state == 1 && current_button == 0 && button_debounce_count == 0
+    puts "BUTTON!"
+    md_uart.write((0x99).chr + 49.chr + (0x7F).chr)
+    group_history[group_history.size - 1] = 5
+    button_debounce_count = 10
   end
-  debounce -= 1 if debounce > 0
+  last_button_state = current_button
+  button_debounce_count -= 1 if button_debounce_count > 0
 
-  if tick % 10 == 0
-    a = accel.acceleration
-    d = ((a[:x].abs + a[:y].abs + a[:z].abs) * 100).to_i - 100
-    sat = (d * d / 20 + 127).clamp(50, 255)
-    bright = ((sat - 127) * 81 / 128 + 30).clamp(15, 111)
+  if tick_count % 10 == 0
+    accel_data = accel_sensor.acceleration
+    ax = (accel_data[:x] * 100).to_i
+    ay = (accel_data[:y] * 100).to_i
+    az = (accel_data[:z] * 100).to_i
+    accel_mag = ax.abs + ay.abs + az.abs
+    delta = accel_mag - 100
+    saturation = (delta * delta / 20 + 127).clamp(50, 255)
+    brightness = ((saturation - 127) * 81 / 128 + 30).clamp(15, 111)
   end
 
-  if hist[-1] == Group[:crash]
-    colors.map! { |c| (c & HUE_MASK) | MAX_BRIGHT }
-    hist[-1] = Group.rotate(tick)
+  if group_history.last == 5
+    puts "<<FLASH BLOCK START>>"
+#    led_strip.flash!(60)
+    led_colors.map! { |c| (c & 0xFF0000) | 0x00FF }
+    group_history[group_history.size - 1] = (tick_count % 4) + 1
+    puts "<<FLASH BLOCK END>>"
   else
-    hsb = (sat << SAT_SHIFT) | bright
-    hist.each { |g| g.apply_leds(colors, (g.hue << HUE_SHIFT) | hsb) }
+    sb = (saturation << 8) | brightness
+    group_history.each do |g|
+      h = HUES[g] << 16 | sb
+      case g
+      when 1
+        10.times { |s|
+          led_colors[(s * 6 + led_offset) % led_colors.size] = h
+          led_colors[(s * 6 + 1 + led_offset) % led_colors.size] = h
+          led_colors[(s * 6 + 2 + led_offset) % led_colors.size] = h
+        }
+      when 2
+        10.times { |s|
+          led_colors[(s * 6 + 3 + led_offset) % led_colors.size] = h
+          led_colors[(s * 6 + 4 + led_offset) % led_colors.size] = h
+          led_colors[(s * 6 + 5 + led_offset) % led_colors.size] = h
+        }
+      when 3
+        12.times { |i| led_colors[(i * 5 + led_offset) % led_colors.size] = h }
+      when 4
+        6.times { |i| led_colors[(i * 10 + led_offset) % led_colors.size] = h }
+      else
+        puts "invalid group #{g},#{HUES[g]},#{h}"
+      end
+    end
   end
-  leds.show_hsb_hex(*colors)
+  puts "#{saturation},#{brightness}"
+  led_strip.show_hsb_hex(*led_colors)
   sleep_ms(1)
 end
